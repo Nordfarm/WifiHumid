759cdb23570280f9414f2e694fc52b19350b633f
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2021-03-13 20:21:28 +0100

    Now talking on the WiFi and over MQTT

--- WifiHumid.ino
+++ WifiHumid.ino
@@ -1,3 +1,20 @@
+//WiFi
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include "arduino_secrets.h" 
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+int status = WL_IDLE_STATUS;     // the WiFi radio's status
+
+//MQTT
+#include <ArduinoMqttClient.h>
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+
+const char broker[] = MQTT_BROKER;
+int        port     = 1883;
+const char topic[]  = "arduino/simple";
 
 //Air temperature/humidity
 #include "DHT.h"
@@ -26,9 +43,65 @@ void setup() {
     if (serialCounter > 10) break;
   }
   Serial.println("WifiHumid initializing...");
+
+
+  
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    //delay(10000);
+    while (WiFi.status() == WL_IDLE_STATUS) {
+      Serial.print(".");
+      delay(100);
+    }
+  }
+  Serial.println();
+  // you're connected now, so print out the data:
+  Serial.print("You're connected to the network");
+  printCurrentNet();
+  printWifiData();
+  Serial.println();
+
+
+  // You can provide a unique client ID, if not set the library uses Arduino-millis()
+  // Each client must have a unique client ID
+  // mqttClient.setId("clientId");
+
+  // You can provide a username and password for authentication
+  // mqttClient.setUsernamePassword("username", "password");
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1);
+  }
+
+  Serial.println("You're connected to the MQTT broker!");
+  Serial.println();
+
+
+  
   
   dht.begin();
-
 
   Serial.println("WifiHumid ready!");
 }
@@ -36,6 +109,8 @@ void setup() {
 void loop() {
   // Wait a few seconds between measurements.
   delay(2000);
+
+  mqttClient.poll();
 
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
@@ -59,6 +134,63 @@ void loop() {
     soilhumid_data[i] = 100*(soil0-soilhumid_data[i])/(soil0-soil100); //Calibrate to percent
     Serial.print(", ");
     Serial.print(soilhumid_data[i]);
+  }
+  Serial.println();
+
+  mqttClient.beginMessage(topic);
+  mqttClient.print("Humid = ");
+  mqttClient.print(soilhumid_data[0]);
+  mqttClient.endMessage();
+  
+}
+
+
+void printWifiData() {
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  Serial.println(ip);
+
+  // print your MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  printMacAddress(mac);
+}
+
+void printCurrentNet() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're attached to:
+  byte bssid[6];
+  WiFi.BSSID(bssid);
+  Serial.print("BSSID: ");
+  printMacAddress(bssid);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  // print the encryption type:
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption, HEX);
+  Serial.println();
+}
+
+void printMacAddress(byte mac[]) {
+  for (int i = 5; i >= 0; i--) {
+    if (mac[i] < 16) {
+      Serial.print("0");
+    }
+    Serial.print(mac[i], HEX);
+    if (i > 0) {
+      Serial.print(":");
+    }
   }
   Serial.println();
 }
